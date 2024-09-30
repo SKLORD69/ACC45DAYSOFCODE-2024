986489f60254e1f3a80904b4e65087a4744de031
SKLORD69 <sahilsharma361004@gmail.com>
2024-09-30 23:04:00 +0530

    DAY-3 BREAKSTICK.c

--- DAY-3 BREAKSTICK.c
+++ DAY-3 BREAKSTICK.c
@@ -2,13 +2,12 @@
 
 int main() {
     int T;
-    scanf("%d", &T);  // Read the number of test cases
+    scanf("%d", &T);
     
     while (T--) {
         long long N, X;
-        scanf("%lld %lld", &N, &X);  // Read N and X for each test case
+        scanf("%lld %lld", &N, &X);
         
-        // If N is even, Chef can break it into parts of any parity (odd or even)
         if (N % 2 == 0) {
             if (X <= N) {
                 printf("YES\n");
@@ -16,7 +15,6 @@ int main() {
                 printf("NO\n");
             }
         }
-        // If N is odd, Chef can only break it into odd parts, so X must be odd
         else {
             if (X <= N && X % 2 == 1) {
                 printf("YES\n");
